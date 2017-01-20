e1cc94ed615a67f582625b701ca4a46daa3bb2c9
Edward Dam <edward.dam@gmail.com>
2017-01-20 08:38:33 +0000

    Initial Commit

---
+++ src/pkjs/config.js
@@ -0,0 +1,33 @@
+// Author: Ed Dam
+
+module.exports = [
+  {
+    "type": "heading",
+    "defaultValue": "News v1.0"
+  },
+  {
+    "type": "text",
+    "defaultValue": "by Edward Dam"
+  },
+  {
+    "type": "section",
+    "items": [
+      {
+        "type": "heading",
+        "defaultValue": "Powered by NewsAPI.org"
+      },
+      {
+        "type": "text",
+        "defaultValue": "https://newsapi.org/"
+      },
+    ]
+  },
+  {
+    "type": "text",
+    "defaultValue": "Thank you for using my watch app."
+  },
+  {
+    "type": "submit",
+    "defaultValue": "Exit"
+  }
+];
